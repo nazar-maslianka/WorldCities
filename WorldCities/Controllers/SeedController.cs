38b45a05b0bdfc5acf6fdc313c5c32753f99f438
nazar-maslianka <nazar.maslyanka1998@gmail.com>
2021-02-21 21:52:22 +0200

    Add project files.

---
+++ WorldCities/Controllers/SeedController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCities.Data;
+
+namespace WorldCities.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeedController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public SeedController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ WorldCities/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities.Data.Models;
+
+namespace WorldCities.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        #region Constructor
+        public ApplicationDbContext() : base()
+        {
+
+        }
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<City>().ToTable("Cities");
+            modelBuilder.Entity<Country>().ToTable("Countries");
+        }
+        #endregion Methods
+
+        #region Properties
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        #endregion Properties
+    }
+}
