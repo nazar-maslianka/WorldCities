81cc787865ee07c96f09876b422cd3dba6c129dd
nazar-maslianka <nazar.maslyanka1998@gmail.com>
2021-02-27 21:02:32 +0200

    Add cities component and city model

---
+++ WorldCities/ClientApp/src/app/cities/cities.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { City } from './city';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-cities',
+  templateUrl: './cities.component.html',
+  styleUrls: ['./cities.component.css']
+})
+export class CitiesComponent implements OnInit {
+
+  public cities: City[];
+  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string) { }
+
+  ngOnInit() {
+    this.http.get<City[]>(this.baseUrl + 'api/Cities')
+      .subscribe(result => {
+        this.cities = result;
+      }, error => console.error(error));
+  }
+
+}
