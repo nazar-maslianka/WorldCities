cfd08c453fe0500fca2844714fa1ccd8c1d5a961
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2021-08-24 18:18:40 +0300

    +> Back-end | Modification in database for storing IdentityUser

--- WorldCities/Data/ApplicationDbContext.cs
+++ WorldCities/Data/ApplicationDbContext.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using WorldCities.Data.Models;
 
 namespace WorldCities.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
         #region Constructor
-        public ApplicationDbContext() : base()
-        {
-
-        }
-
-        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions
+            ) : base(options, operationalStoreOptions)
         {
 
 
