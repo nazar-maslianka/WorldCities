43b46a9a94874efcf11087ff1b4a40b5fa9e02be
Nazar Maslianka <nazar.maslyanka1998@gmail.com>
2021-08-24 18:21:45 +0300

    +> Back-end | Additional options for Identity user in Startup and new packages

--- WorldCities/Startup.cs
+++ WorldCities/Startup.cs
@@ -1,12 +1,13 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using WorldCities.Data;
+using WorldCities.Data.Models;
 
 namespace WorldCities
 {
@@ -22,9 +23,10 @@ namespace WorldCities
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews()
+            services.AddControllersWithViews().AddNewtonsoftJson()
                 .AddJsonOptions(options => {
                     options.JsonSerializerOptions.WriteIndented = true;
+                    options.JsonSerializerOptions.PropertyNamingPolicy = null;
                 });
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -37,6 +39,25 @@ namespace WorldCities
                     Configuration.GetConnectionString("DefaultConnection")
                     )
                 );
+
+            // Add ASP.NET Core Identity support
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = true;
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequireNonAlphanumeric = true;
+                    options.Password.RequiredLength = 8;
+                })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+            
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
 
             services.AddCors(opt =>
             {
@@ -69,6 +90,10 @@ namespace WorldCities
             }
             app.UseCors("CorcPolicy");
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseIdentityServer();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
